6c766b7aa5edc3b666e50b66285fbd79805649ed
virush13 <hestroncio@gmail.com>
2016-03-29 22:34:08 +0200

    listado
    listado asp.net

---
+++ MvcApplication2/Controllers/PersonaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Models;
+
+namespace MvcApplication2.Controllers
+{
+    public class PersonaController : Controller
+    {
+        //
+        // GET: /Persona/
+
+        public ActionResult Index()
+        {
+            List<Persona> lista= new List<Persona>();
+            Persona per1 = new Persona();
+            per1.codigo = 1;
+            per1.nombre = "luis";
+            lista.Add(per1);
+            Persona per2 = new Persona();
+            per2.codigo = 2;
+            per2.nombre = "luis2";
+            lista.Add(per2);
+            Persona per3 = new Persona();
+            per3.codigo = 3;
+            per3.nombre = "luis3";
+            lista.Add(per3);
+            return View(lista);
+        }
+
+    }
+}
---
+++ MvcApplication2/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class Persona
+    {
+        public int codigo{get; set;}
+        public String nombre { get; set; }
+    }
+}
